d208dda2085615b48b37c074b21dd5ccc732d070
Chidera Ozoagu <deraozoagu@gmail.com>
2022-06-24 23:12:32 +0100

    changes to css and js

--- app.js
+++ app.js
@@ -1,29 +1,49 @@
 const sidebarToggle = document.querySelector(".sidebar-toggle");
 const closeBtn = document.querySelector(".close-btn");
 const sidebar = document.querySelector(".sidebar");
+const overlay = document.querySelector('.aside-overlay');
 
 // sidebar toggle
 sidebarToggle.addEventListener("click", function () {
-  sidebar.classList.toggle("show-sidebar");
+  sidebar.classList.add("show-sidebar");
+
+  if (sidebar.classList.contains('show-sidebar')) {
+    overlay.classList.add('show-overlay');
+  } 
+
 });
+
+// close sidebar with overlay
+overlay.addEventListener('click', ()=>{
+  if (overlay){
+    sidebar.classList.remove('show-sidebar');
+    overlay.classList.remove('show-overlay');
+  }
+})
 
 // close sidebar
 closeBtn.addEventListener("click", function () {
   sidebar.classList.remove("show-sidebar");
+  if(closeBtn) {
+    overlay.classList.remove('show-overlay');
+  }
+
 });
 
 // nested links
 const nestedLinks = document.querySelector(".nested-links");
 const nested = document.querySelectorAll(".nested");
-
+const arrowBtn = document.querySelector('.arrow-btn')
 nested.forEach(function (items) {
-  items.addEventListener("click", function (e) {
-    const btn = e.currentTarget.lastChild.previousSibling;
-    console.log(btn);
+  items.addEventListener("click", function () {
+    const btn = items.lastChild.previousSibling;
     if (btn.classList.contains("features")) {
       btn.classList.toggle("show-features");
     } else {
       btn.classList.toggle("show-company");
     }
+   
   });
+  
 });
+
